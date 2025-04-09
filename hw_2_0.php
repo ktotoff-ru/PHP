4a972a3ee7ea1c6cb9acf57a50ad556101055724
Andrew Savelyev <ktotoff@gmail.com>
2025-04-09 04:00:11 +0300

    Add HW 2.0

---
+++ hw_2_0.php
@@ -0,0 +1,47 @@
+<?php
+
+# --------------------------------------------------
+# ДОМАШНЕЕ ЗАДАНИЕ #2
+# --------------------------------------------------
+
+// First
+
+do {
+    $a = trim(readline( "Первое число: "));
+
+    // int
+
+    if ( !is_numeric($a) ) {
+        fwrite(STDERR, "Введите, пожалуйста, число! ");
+    }
+    
+} while ( !ctype_digit($a) );
+
+// Second
+
+do {
+    
+    $b = trim(readline("Второе число: "));
+
+    // int
+
+    if ( !ctype_digit($b) ) {
+        fwrite(STDERR, "Введите, пожалуйста, число! ");
+    }
+
+
+    // zero
+
+    if ( ctype_digit($b) && (int) $b === 0 ) {
+        fwrite(STDERR, "Делить на 0 нельзя! ");
+    }
+
+} while ( !ctype_digit($b) || ( ctype_digit($b) && (int) $b === 0 ) ) ;
+
+// Result
+
+echo "------------------"  . PHP_EOL ;
+echo "Результат деления: $a / $b = " . $a / $b . PHP_EOL;
+echo "\n\n" ;
+
+?>
